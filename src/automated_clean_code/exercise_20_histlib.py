7040bedd3d6d23a1ba5e09696d23ec1a1a567ea8
BlackTea13 <robertblum2001@gmail.com>
2023-01-27 17:06:42 +0700

    added MinMaxObject to hold keys of dictionary, changed tests to accomodate

--- src/automated_clean_code/exercise_20_histlib.py
+++ src/automated_clean_code/exercise_20_histlib.py
@@ -1,10 +1,30 @@
 # For this exercise focus on how to testability. How do we test thing like this?
 # and test fixture
 # the example data is in data/exercise20_data.txt
-from typing import Dict, Iterable, Tuple
+from dataclasses import dataclass
+from typing import Dict, Iterable, Optional
 
 
-def find_freq_from_list(lines: Iterable[str]) -> Dict:
+@dataclass
+class MinMaxObject:
+    """
+    A class to hold min and max keys.
+
+    ...
+
+    Attributes
+    ----------
+    min : str
+        key of minimum value
+    max : str
+        key of maximum value
+    """
+
+    min: str
+    max: str
+
+
+def find_freq_from_list(lines: Iterable[str]) -> Optional[Dict]:
     """Find frequency of each string in a list.
 
     Args:
@@ -12,6 +32,7 @@ def find_freq_from_list(lines: Iterable[str]) -> Dict:
 
     Returns:
       (Dict). a dictionary with the frequency of each string
+      (None). none if input is empty
     """
     frequencies = {}
     for line in lines:
@@ -19,10 +40,13 @@ def find_freq_from_list(lines: Iterable[str]) -> Dict:
             frequencies[line] = 1
         elif line in frequencies.keys():
             frequencies[line] += 1
+
+    if frequencies == {}:
+        return None
     return frequencies
 
 
-def get_min_max_freq_from_dict(dictionary: Dict) -> Tuple[str, str] | None:
+def get_min_max_freq_from_dict(dictionary: Dict) -> Optional[MinMaxObject]:
     """Find keys with min and max values from a dictionary.
 
     Args:a
@@ -30,13 +54,14 @@ def get_min_max_freq_from_dict(dictionary: Dict) -> Tuple[str, str] | None:
 
     Returns:
       (Tuple[str, str]). A tuple of the min and max key
+      (None). Returns none if input is empty
     """
     if len(dictionary.keys()) == 0:
         return None
-    return max(dictionary, key=dictionary.get), min(dictionary, key=dictionary.get)
+    return MinMaxObject(max=max(dictionary, key=dictionary.get), min=min(dictionary, key=dictionary.get))
 
 
-def hist_lib(filename: str) -> Tuple[str, str]:
+def hist_lib(filename: str) -> Optional[MinMaxObject]:
     """Find keys with min and max values from a file.
 
     Args:a
--- tests/test_histlib.py
+++ tests/test_histlib.py
@@ -1,4 +1,4 @@
-from automated_clean_code.exercise_20_histlib import find_freq_from_list, get_min_max_freq_from_dict
+from automated_clean_code.exercise_20_histlib import MinMaxObject, find_freq_from_list, get_min_max_freq_from_dict
 
 
 def test_freq_from_list():
@@ -10,7 +10,7 @@ def test_freq_from_list():
     output1 = {"apple": 3, "banana": 2, "orange": 1}
     output2 = {"apple": 4, "banana": 2, "orange": 1}
     output3 = {"apple": 4, "banana": 3, "orange": 1}
-    output4 = {}
+    output4 = None
 
     assert find_freq_from_list(input1) == output1
     assert find_freq_from_list(input2) == output2
@@ -23,8 +23,8 @@ def test_get_min_max_from_dict():
     input2 = {"oreo": 3}
     input3 = {}
 
-    output1 = ("apple", "orange")
-    output2 = ("oreo", "oreo")
+    output1 = MinMaxObject(max="apple", min="orange")
+    output2 = MinMaxObject(max="oreo", min="oreo")
     output3 = None
 
     assert get_min_max_freq_from_dict(input1) == output1
