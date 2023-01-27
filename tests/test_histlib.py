fa1795d8bc817c08cc1ffd0ec30eaa4158bdb947
BlackTea13 <robertblum2001@gmail.com>
2023-01-27 16:31:09 +0700

    fixed broken test case in min max from dict

--- tests/test_histlib.py
+++ tests/test_histlib.py
@@ -23,7 +23,7 @@ def test_get_min_max_from_dict():
     input2 = {"oreo": 3}
 
     output1 = ("apple", "orange")
-    output2 = {"oreo", "oreo"}
+    output2 = ("oreo", "oreo")
 
-    assert get_min_max_freq_from_dict(input1), output1
-    assert get_min_max_freq_from_dict(input2), output2
+    assert get_min_max_freq_from_dict(input1) == output1
+    assert get_min_max_freq_from_dict(input2) == output2
